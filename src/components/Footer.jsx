4e4a782a03d1dfe223fc166143a5b056c1d3938a
thebalazsoliver <balazsoliver.hu@gmail.com>
2026-07-06 10:55:29 +0200

    fix footer logo and favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom'
-import logo from '../assets/logo-mark.svg'
 import './Footer.css'
 
 export default function Footer() {
   const year = new Date().getFullYear()
+
   return (
     <footer className="footer">
       <div className="container footer__inner">
         <div className="footer__brand">
-          <img src={logo} alt="" className="footer__mark" />
+          <img
+            src="/prairie-pros-logo.svg"
+            alt="Prairie Pros Exterior Services"
+            className="footer__mark"
+          />
+
           <div>
             <p className="footer__name">PRAIRIE PROS</p>
             <p className="footer__tag">Exterior Services</p>
@@ -17,25 +22,44 @@ export default function Footer() {
 
         <div className="footer__col">
           <p className="footer__heading">Get in touch</p>
-          <a href="tel:+17012609707" className="footer__link">(701) 260-9707</a>
-          <a href="/#contact" className="footer__link">Request a quote</a>
+          <a href="tel:+17012609707" className="footer__link">
+            (701) 260-9707
+          </a>
+          <a href="/#contact" className="footer__link">
+            Request a quote
+          </a>
         </div>
 
         <div className="footer__col">
           <p className="footer__heading">Explore</p>
-          <Link to="/" className="footer__link">Home</Link>
-          <Link to="/pricing" className="footer__link">Pricing</Link>
-          <Link to="/faq" className="footer__link">FAQ</Link>
+          <Link to="/" className="footer__link">
+            Home
+          </Link>
+          <Link to="/pricing" className="footer__link">
+            Pricing
+          </Link>
+          <Link to="/faq" className="footer__link">
+            FAQ
+          </Link>
         </div>
 
         <div className="footer__col">
           <p className="footer__heading">Services</p>
-          <a href="/#services" className="footer__link">Snow removal</a>
-          <a href="/#services" className="footer__link">Pressure washing</a>
-          <a href="/#services" className="footer__link">Holiday lighting</a>
-          <a href="/#services" className="footer__link">Fleet &amp; bin washing</a>
+          <a href="/#services" className="footer__link">
+            Snow removal
+          </a>
+          <a href="/#services" className="footer__link">
+            Pressure washing
+          </a>
+          <a href="/#services" className="footer__link">
+            Holiday lighting
+          </a>
+          <a href="/#services" className="footer__link">
+            Fleet &amp; bin washing
+          </a>
         </div>
       </div>
+
       <div className="footer__bottom">
         <div className="container footer__bottom-inner">
           <p>© {year} Prairie Pros LLC. Built to weather every season.</p>
